4c57c2a60c6218526160979267346d5337ed3146
mowshe <6mikeski13@gmail.com>
2019-08-12 12:48:48 -0500

    Added stats to state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FetchUser from './components/FetchUser.js'
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <FetchUser/>
+        <h1>VS</h1>
+        <FetchUser/>
+
       </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FetchUser.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+class FetchUser extends React.PureComponent {
+
+
+  state = {
+    loading:true,
+    person:null,
+    min:5,
+    max:100,
+    hp: null,
+    attack:null,
+    Defense:null
+  };
+
+  async componentDidMount() {
+    const url = "https://api.randomuser.me/";
+    const response = await fetch(url);
+    const data = await response.json();
+    //const hp = "ahh";
+    const hp = Math.floor(Math.random() * (+this.state.max - +this.state.min)) + +this.state.min;
+    const attack = Math.floor(Math.random() * (+this.state.max - +this.state.min)) + +this.state.min;
+    const defense = Math.floor(Math.random() * (+this.state.max - +this.state.min)) + +this.state.min;
+    this.setState({
+      person: data.results[0],
+      loading: false,
+      hp: hp,
+      attack: attack,
+      defense: defense
+    });
+  }
+
+  render() {
+    return (
+
+      <div>
+        {this.state.loading || !this.state.person ? (
+          <div>loading...</div>
+        ):(
+          <div>
+            <img src={this.state.person.picture.large} alt="ah" />
+            <div>{this.state.person.name.first}</div>
+            <div>{this.state.person.name.last}</div>
+            <div>HP: {this.state.hp}</div>
+            <div>Attack: {this.state.attack}</div>
+            <div>Defense: {this.state.defense}</div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default FetchUser;
